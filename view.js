9adebf304d78d81f00a26481b8baaf7defa42729
billy <wmjhappy_ok@126.com>
2017-02-21 09:57:44 +0800

    将日志输出组件以源码形式附在正文中

--- view.js
+++ view.js
@@ -9,8 +9,190 @@
  */
 ;(function(ctx, name){
 	/** 日志输出组件 */
-	var Logger = (function(){var f=function(){if(arguments.length==0){return}var m=arguments;var n=" "+String(arguments[0]);var l=1;n=n.replace(/([^\\])\{\}/g,function(p,o){var q=(m.length-1)<l?"{}":String(h(m[l]));l++;return o+q});n=n.replace(/\\\{\}/g,"{}");return n.substring(1)};var d=function(){var q=new Date();var o=q.getFullYear();var p="0"+String(q.getMonth()+1);p=p.substring(p.length-2);var m="0"+String(q.getDate());m=m.substring(m.length-2);var l="0"+String(q.getHours());l=l.substring(l.length-2);var n="0"+String(q.getMinutes());n=n.substring(n.length-2);var r="0"+String(q.getSeconds());r=r.substring(r.length-2);return p+m+" "+l+":"+n+":"+r};var h=function(o){if(null==o){return null}if((typeof o=="number")||(typeof o=="string")||(typeof o=="boolean")){return o}else{if(typeof o=="function"){return"function "+o.name+"(){...}"}else{if(!(o instanceof Array)&&(typeof o=="object")){if(String(o)!="[object Object]"){return String(o)}else{try{var l={};for(var n in o){l[n]=h(o[n])}return JSON.stringify(l)}catch(m){return String(o)}}}else{if(o instanceof Array){return JSON.stringify(o.map(function(p){return h(p)}))}else{return o}}}}};var i=function(l){return d()+" ["+l+"]: "};var c={};var j,e;(function(){var l=true;j=function(){return l};e=function(m){l=m}})();var b=function(l){var m=true;this.isEnabled=function(){return j()&&m};this.setIsEnabled=function(n){m=n;return this};this.getName=function(){return l};this.debug=function(){if(!this.isEnabled()){return}var n=f.apply(null,arguments);console.debug(i(l)+n)};this.info=function(){if(!this.isEnabled()){return}var n=f.apply(null,arguments);console.info(i(l)+n)};this.warn=function(){if(!this.isEnabled()){return}var n=f.apply(null,arguments);console.warn(i(l)+n)};this.error=function(){if(!this.isEnabled()){return}var n=f.apply(null,arguments);console.error(i(l)+n)};this.log=function(){if(!this.isEnabled()){return}var n=f.apply(null,arguments);console.log(i(l)+n)}};var g=function(m){if(m in c){return c[m]}var l=new b(m);c[m]=l;return l};b.ofName=g;b.isGloballyEnabled=j;b.setIsGloballyEnabled=e;return b;})();
+	var Logger = (function(){
+		/**
+		 * 填充字符串中的占位符："{}"。使用“\\”进行转义，以输出字符串：“{}”
+		 * @param {String} arguments[0] 包含有占位符的源字符串
+		 * @param {Any} arguments[1...n] 占位符取值。将使用String()方法转换输出
+		 */
+		var fillPlaceholders = function(){
+			if(arguments.length == 0)
+				return;
 
+			var args = arguments;
+			var str = " " + String(arguments[0]);
+			var index = 1;
+			str = str.replace(/([^\\])\{\}/g, function($0, $1){
+				var value = (args.length - 1) < index? "{}": String(getNotion(args[index]));
+				index++;
+				return $1 + value;
+			});
+			str = str.replace(/\\\{\}/g, "{}");
+			return str.substring(1);
+		};
+
+		/** 获取当前时间 */
+		var getTime = function(){
+			var time = new Date();
+			var year = time.getFullYear();
+			var month =  "0" + String(time.getMonth() + 1);
+			month = month.substring(month.length - 2);
+			var date =  "0" + String(time.getDate());
+			date = date.substring(date.length - 2);
+			var hours = "0" + String(time.getHours());
+			hours = hours.substring(hours.length - 2);
+			var minutes = "0" + String(time.getMinutes());
+			minutes = minutes.substring(minutes.length - 2);
+			var seconds = "0" + String(time.getSeconds());
+			seconds = seconds.substring(seconds.length - 2);
+
+			return month + date + " " + hours + ":" + minutes + ":" + seconds;
+		};
+
+		/**
+		 * 获取指定对象的简略表达方式
+		 */
+		var getNotion = function(obj){
+			if(null == obj)
+				return null;
+
+			if((typeof obj == "number") || (typeof obj == "string") || (typeof obj == "boolean"))
+				return obj;
+			else if(typeof obj == "function")
+				return "function " + obj.name + "(){...}";
+			else if(!(obj instanceof Array) && (typeof obj == "object")){
+				if(String(obj) != "[object Object]")
+					return String(obj);
+				else{
+					try{
+						var tmp = {};
+						for(var p in obj)
+							tmp[p] = getNotion(obj[p]);
+						return JSON.stringify(tmp);
+					}catch(e){
+						return String(obj);
+					}
+				}
+			}else if(obj instanceof Array)
+				return JSON.stringify(obj.map(function(ele){
+					return getNotion(ele);
+				}));
+			else
+				return obj;
+		};
+
+		/** 获取日志前缀 */
+		var getLogPrefix = function(name){
+			return getTime() + " [" + name + "]: ";
+		};
+
+		var loggers = {};
+
+		/** 获取，及设置日志输出状态全局状态的方法 */
+		var isGloballyEnabled, setIsGloballyEnabled;
+		;(function(){
+			var flag = true;
+
+			isGloballyEnabled = function(){
+				return flag;
+			};
+
+			setIsGloballyEnabled = function(v){
+				flag = v;
+			};
+		})();
+
+		/**
+		 * @constructor
+		 * 日志输出组件
+		 */
+		var Logger = function(name){
+			var isEnabled = true;
+
+			/**
+			 * 判断日志组件是否启用
+			 * @return {boolean} 当前对象日志输出是否启用
+			 */
+			this.isEnabled = function(){
+				return isGloballyEnabled() && isEnabled;
+			};
+
+			/**
+			 * 设置日志输出的启用状态
+			 * @param {Boolean} v 是否启用
+			 */
+			this.setIsEnabled = function(v){
+				isEnabled = v;
+				return this;
+			};
+
+			/** 获取名称 */
+			this.getName = function(){
+				return name;
+			};
+
+			this.debug = function(){
+				if(!this.isEnabled())
+					return;
+
+				var msg = fillPlaceholders.apply(null, arguments);
+				console.debug(getLogPrefix(name) + msg);
+			};
+
+			this.info = function(){
+				if(!this.isEnabled())
+					return;
+
+				var msg = fillPlaceholders.apply(null, arguments);
+				console.info(getLogPrefix(name) + msg);
+			};
+
+			this.warn = function(){
+				if(!this.isEnabled())
+					return;
+
+				var msg = fillPlaceholders.apply(null, arguments);
+				console.warn(getLogPrefix(name) + msg);
+			};
+
+			this.error = function(){
+				if(!this.isEnabled())
+					return;
+
+				var msg = fillPlaceholders.apply(null, arguments);
+				console.error(getLogPrefix(name) + msg);
+			};
+
+			this.log = function(){
+				if(!this.isEnabled())
+					return;
+
+				var msg = fillPlaceholders.apply(null, arguments);
+				console.log(getLogPrefix(name) + msg);
+			};
+		};
+
+		/**
+		 * 获取指定名称的logger
+		 * @param {String} name 名称
+		 */
+		var ofName = function(name){
+			if(name in loggers)
+				return loggers[name];
+
+			var instance = new Logger(name);
+			loggers[name] = instance;
+
+			return instance;
+		};
+		Logger.ofName = ofName;
+
+		Logger.isGloballyEnabled = isGloballyEnabled;
+		Logger.setIsGloballyEnabled = setIsGloballyEnabled;
+
+		return Logger;
+	})();
+	
 	/** 触摸支持组件 */
 	var touch = (function(){
 		/* 添加的依附于DOM元素的，用于记录其相关取值的属性名称 */
