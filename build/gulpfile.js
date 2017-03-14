ccb4f64261a9bf0fb78022575bec877956fcc6dc
billy <wmjhappy_ok@126.com>
2017-03-14 15:42:13 +0800

    添加gulp构建支持

---
+++ build/gulpfile.js
@@ -0,0 +1,66 @@
+var gulp = require("gulp"),
+	rename = require('gulp-rename'),
+	uglify = require("gulp-uglify"),
+	cleanCss = require("gulp-clean-css"),
+	zip = require("gulp-zip"),
+	del = require("del");
+
+var version = "1.4.1";
+
+/** 获取当前时间 */
+var getTime = function(){
+	var time = new Date();
+	var year = time.getFullYear();
+	var month =  "0" + String(time.getMonth() + 1);
+	month = month.substring(month.length - 2);
+	var date =  "0" + String(time.getDate());
+	date = date.substring(date.length - 2);
+	var hours = "0" + String(time.getHours());
+	hours = hours.substring(hours.length - 2);
+	var minutes = "0" + String(time.getMinutes());
+	minutes = minutes.substring(minutes.length - 2);
+	
+	return year + month + date + hours + minutes;
+};
+
+var getVersion = function(){
+	return version + "-B" + getTime();
+};
+
+var min = function(){
+	gulp.src('../view.js')
+		.pipe(rename("view." + getVersion() + ".min.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest('../'));
+};
+
+var copySource = function(version){
+	gulp.src('../view.js')
+		.pipe(rename("view." + version + ".js"))
+        .pipe(gulp.dest('../release/' + version));
+	
+	gulp.src('../view.css')
+		.pipe(rename("view." + version + ".css"))
+        .pipe(gulp.dest('../release/' + version));
+};
+
+var copyMin = function(version){
+	gulp.src('../view.js')
+		.pipe(rename("view." + version + ".min.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest('../release/' + version));
+
+	gulp.src('../view.css')
+		.pipe(rename("view." + version + ".min.css"))
+        .pipe(cleanCss())
+        .pipe(gulp.dest('../release/' + version));
+};
+
+var stage = function(){
+	var version = getVersion();
+	copySource(version);
+	copyMin(version);
+};
+
+gulp.task('min', min);
+gulp.task('stage', stage);
---
+++ build/install-gulp.bat
@@ -0,0 +1,6 @@
+call npm install --save-dev gulp
+call npm install --save-dev gulp-rename
+call npm install --save-dev gulp-uglify
+call npm install --save-dev gulp-clean-css
+call npm install --save-dev gulp-zip@3.2.0
+call npm install --save-dev del
