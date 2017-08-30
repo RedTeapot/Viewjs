62e990725e0fdece2c834ef6b437e2250355795b
17603860848 <wmjhappy_ok@126.com>
2017-08-30 10:37:50 +0800

    修复故障：源视图的leave事件出发时，如果监听器报错，则新视图的enter等事件将无法触发；修复故障：如果默认视图与页面打开后要呈现的第一个视图相同，则该视图在触发leave事件后又出发enter事件

--- docs/web/www/js/page/define.version.js
+++ docs/web/www/js/page/define.version.js
@@ -1,4 +1,4 @@
 ;(function(){
-	window.viewJsNewestVersion = "1.5.0-B201708291125";
-	window.viewJsNewestZipFile = "dist/viewjs-1.5.0-B201708291125.zip";
+	window.viewJsNewestVersion = "1.5.0-B201708301029";
+	window.viewJsNewestZipFile = "dist/viewjs-1.5.0-B201708301029.zip";
 })();
