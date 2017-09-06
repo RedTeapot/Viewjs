0ebfa5cb5c73214a259835c9c0bf937867f426b8
17603860848 <wmjhappy_ok@126.com>
2017-09-06 10:25:24 +0800

    1. data-view-fallback支持伪视图：:default-view；2. data-view-fallback支持视图群组；3. setViewId(viewId)支持伪视图：:default-view；4. setViewId(viewId)支持视图群组

--- docs/web/www/js/page/define.version.js
+++ docs/web/www/js/page/define.version.js
@@ -1,4 +1,4 @@
 ;(function(){
-	window.viewJsNewestVersion = "1.5.0-B201709051136";
-	window.viewJsNewestZipFile = "dist/viewjs-1.5.0-B201709051136.zip";
+	window.viewJsNewestVersion = "1.5.0-B201709061010";
+	window.viewJsNewestZipFile = "dist/viewjs-1.5.0-B201709061010.zip";
 })();
