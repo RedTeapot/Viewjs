f13fea0e8dd84fd38afebe219e641ce970fcbf9b
17603860848 <wmjhappy_ok@126.com>
2017-10-29 16:19:15 +0800

    1.5.1-B201710291615发布：修复故障：对于可直接访问的视图V，如果页面打开后在View.ready事件中切换至视图V，则在直接访问视图V时，视图V的enter事件被触发两次

--- docs/web/www/js/page/define.version.js
+++ docs/web/www/js/page/define.version.js
@@ -1,4 +1,4 @@
 ;(function(){
-	window.viewJsNewestVersion = "1.5.1-B201710171133";
-	window.viewJsNewestZipFile = "dist/viewjs-1.5.1-B201710171133.zip";
+	window.viewJsNewestVersion = "1.5.1-B201710291615";
+	window.viewJsNewestZipFile = "dist/viewjs-1.5.1-B201710291615.zip";
 })();
