7937037f738d4692f03d51f27d2876274d743bb5
17603860848 <wmjhappy_ok@126.com>
2017-12-21 17:03:58 +0800

    修复故障：当地址栏中指定页面打开后要呈现的第一个视图时，页面打开后，View.ready方法的执行顺序晚于第一个视图的enter方法

--- docs/web/www/js/page/define.version.js
+++ docs/web/www/js/page/define.version.js
@@ -1,4 +1,4 @@
 ;(function(){
-	window.viewJsNewestVersion = "1.5.1-B201712141454";
-	window.viewJsNewestZipFile = "dist/viewjs-1.5.1-B201712141454.zip";
+	window.viewJsNewestVersion = "1.5.1-B201712211701";
+	window.viewJsNewestZipFile = "dist/viewjs-1.5.1-B201712211701.zip";
 })();
