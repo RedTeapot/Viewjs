6480f32ace13aad2dbab2998f84f41538d70b0be
17603860848 <wmjhappy_ok@126.com>
2018-01-25 13:47:28 +0800

    修复故障：当通过API：View.navTo跳转视图，且携带视图选项不携带视图参数时，enter监听句柄中无法获取到视图选项

--- docs/web/www/js/page/define.version.js
+++ docs/web/www/js/page/define.version.js
@@ -1,4 +1,4 @@
 ;(function(){
-	window.viewJsNewestVersion = "1.5.1-B201801041047";
-	window.viewJsNewestZipFile = "dist/viewjs-1.5.1-B201801041047.zip";
+	window.viewJsNewestVersion = "1.5.1-B201801251344";
+	window.viewJsNewestZipFile = "dist/viewjs-1.5.1-B201801251344.zip";
 })();
