6bd618e7748fc832e540c6689ca49686098d859c
17603860848 <wmjhappy_ok@126.com>
2018-05-29 20:19:57 +0800

    【1.5.2-B201805292016】1. 优化日志输出；2. 部分故障修复；3. 文档添加GitHub链接

--- docs/web/www/js/page/define.version.js
+++ docs/web/www/js/page/define.version.js
@@ -1,4 +1,4 @@
 ;(function(){
-	window.viewJsNewestVersion = "1.5.2-B201802081353";
-	window.viewJsNewestZipFile = "dist/viewjs-1.5.2-B201802081353.zip";
+	window.viewJsNewestVersion = "1.5.2-B201805292016";
+	window.viewJsNewestZipFile = "dist/viewjs-1.5.2-B201805292016.zip";
 })();
