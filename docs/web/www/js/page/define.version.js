ce1925d78817441cb0a51b8caa3a400ed270f063
17603860848 <wmjhappy_ok@126.com>
2018-07-24 10:02:27 +0800

    add forum link

--- docs/web/www/js/page/define.version.js
+++ docs/web/www/js/page/define.version.js
@@ -1,4 +1,4 @@
 ;(function(){
-	window.viewJsNewestVersion = "1.5.2-B201805292016";
-	window.viewJsNewestZipFile = "dist/viewjs-1.5.2-B201805292016.zip";
+	window.viewJsNewestVersion = "1.5.3-B201807240943";
+	window.viewJsNewestZipFile = "dist/viewjs-1.5.3-B201807240943.zip";
 })();
