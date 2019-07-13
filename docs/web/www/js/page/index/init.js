2c2cee3ab17a3d86960e4758b5c83ed471a0fa67
17603860848 <wmjhappy_ok@126.com>
2019-07-13 18:45:11 +0800

    集成gittalk

--- docs/web/www/js/page/index/init.js
+++ docs/web/www/js/page/index/init.js
@@ -23,10 +23,11 @@
 	var gitalk = new Gitalk({
 		clientID: '408f27eb84b2b896c507',
 		clientSecret: '0e5d4097e6817656f2b33d33139edc3e17733c3c',
-		repo: 'Viewjs#1',
+		repo: 'Viewjs',
 		owner: 'RedTeapot',
-		admin: ['HotTeapot'],
-		id: location.pathname,      // Ensure uniqueness and length less than 50
+		admin: ['RedTeapot'],
+		labels: ['GitTalk'],
+		id: md5(location.pathname),      // Ensure uniqueness and length less than 50
 		distractionFreeMode: false  // Facebook-like distraction free mode
 	});
 	gitalk.render(bodyObj);
